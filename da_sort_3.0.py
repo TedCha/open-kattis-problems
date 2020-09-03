edce2a6f8475b98d604f0a139d047c15ef96f50e
Theodore Charts <tedcharts@gmail.com>
2020-09-02 22:20:06 -0400

    da_sort finished

---
+++ da_sort_3.0.py
@@ -0,0 +1,28 @@
+# https://open.kattis.com/problems/dasort
+# p = number of datasets
+# k = data set number
+# n = length of array
+# positive integers only
+# 10 values per line, except for last line which may have less than 10 values
+
+import math
+
+p = int(input())
+
+for _ in range(p):
+    k, n = map(int, input().split(' '))
+    
+    j = math.ceil(n / 10)
+    l = []
+
+    for _ in range(j):
+        l += [int(x) for x in input().split(' ')]
+    
+    s = sorted(l)
+
+    si = 0
+
+    for item in l:
+        if item == s[si]:
+            si += 1
+    print(f'{k} {len(l) - si}')
