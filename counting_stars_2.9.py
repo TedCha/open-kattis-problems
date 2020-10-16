f39632074401607d288d1725f7431d872816fea0
Theodore Charts <tedcharts@gmail.com>
2020-10-16 17:36:59 -0400

    counting stars - bfs!

---
+++ counting_stars_2.9.py
@@ -0,0 +1,52 @@
+# https://open.kattis.com/problems/countingstars
+
+from collections import deque
+from sys import stdin
+
+caseCount = 0
+
+for line in stdin:
+
+    caseCount += 1
+    
+    x, y = [int(x) for x in line.split()]
+
+    graph = [input() for _ in range(x)]
+
+    starCount = 0
+
+    visited = set()
+
+    def validMoves(node):
+        r, c = node[0], node[1]
+
+        validMoves = []
+
+        for move in [(r, c-1), (r, c+1), (r-1, c), (r+1, c)]:
+            y, x = move[0], move[1]
+            if not (0 <= x < len(graph[0])) or not (0 <= y < len(graph)):
+                continue
+            else:
+                if graph[y][x] == "-":
+                    validMoves.append(move)
+        return validMoves
+
+    def breadthFirstSearch(node):
+        queue = deque([node])
+        visited.add(node)
+
+        while queue:
+            node = queue.popleft()
+            for move in validMoves(node):
+                if move not in visited:
+                    visited.add(move)
+                    queue.append(move)
+            
+    for k in range(len(graph)):
+        for j in range(len(graph[k])):
+            point = (k, j)
+            if (graph[k][j] == "-") and (point not in visited):
+                breadthFirstSearch(point)
+                starCount += 1
+
+    print(f"Case {caseCount}: {starCount}")
