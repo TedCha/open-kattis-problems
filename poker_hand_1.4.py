559367d455e6219300140c38f3e8686154bedb1c
Theodore Charts <tedcharts@gmail.com>
2020-11-20 13:06:40 -0500

    poker hand rework, and server problem added

--- poker_hand_1.4.py
+++ poker_hand_1.4.py
@@ -1,4 +1,32 @@
 # https://open.kattis.com/problems/pokerhand
 
 # It's so ugly it's beautiful
-string = input().replace(" ", "")[::2]; print(max(dict((letter, string.count(letter)) for letter in set(string)).values()))
+h = input()[::3]; print(max({c: h.count(c) for c in set(h)}.values()))
+
+"""
+# Explanation:
+# Creates a string and then slices every 3 characters to get only the value of the card
+ranksInHand = input()[::3]
+# Input: AC AD AH AS KD
+# Output: AAAAK
+# Transformation: AC AD AH AS KD -> AAAAK
+
+# Gets the unique ranks in hand
+uniqueRanks = set(ranksInHand)
+# Input: AAAAK
+# Output: {A, K}
+
+# Create a count of each unique rank in the hand
+# .count() is a string method to count how many times a character appears in a string
+countByUniqueRank = {rank: ranksInHand.count(rank) for rank in uniqueRanks}
+# Input: uniqueRanks == {A, K}
+# Output: countByUniqueRank == {'A': 4, 'K': 1}
+
+# Get the max value from all unique rank values
+# .values() returns an object (<class 'dict_values'>: a Dictionary View Object)
+# https://docs.python.org/3/library/stdtypes.html#dictionary-view-objects <- Documentation
+maxValue = max(countByUniqueRank.values())
+
+print(maxValue)
+"""
+
--- server_1.7.py
+++ server_1.7.py
@@ -5,5 +5,5 @@ n, t = [int(x) for x in input().split()]
 tasks = [int(x) for x in input().split()]
 
 # Local Variable in List Comprehension (https://stackoverflow.com/questions/26672532/how-to-set-local-variable-in-list-comprehension)
-# If only kattis used python 3.8!
+# If only kattis used python 3.8! (:=)
 print([j for j in (sum(tasks[:(l+1)]) for l, m in enumerate(tasks)) if j <= t])
