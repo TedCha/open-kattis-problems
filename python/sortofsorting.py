fb6fcf88b7d9eb79452e5c4962cff4850d8621da
Theodore Charts <tedcharts@gmail.com>
2021-03-06 23:37:01 -0500

    py: sortofsorting

---
+++ python/sortofsorting.py
@@ -0,0 +1,17 @@
+from sys import stdin
+
+for line in stdin:
+  numberOfValues = int(line)
+
+  if numberOfValues == 0:
+    break
+  
+  nameList = []
+  for i in range(numberOfValues):
+    name = str(input())
+    nameList.append(name)
+
+    nameListInOrder = sorted(nameList, key=lambda name: name[:2])
+
+  print(*nameListInOrder, sep="\n")
+    
